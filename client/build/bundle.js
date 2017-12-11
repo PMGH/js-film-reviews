d9cef3b2d82515281fe7ab8cd315c7078796ecf5
Peter McCready <petermccready2@gmail.com>
2017-12-11 19:56:28 +0000

    Passes genre test with no reviews or genres

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -132,7 +132,7 @@ window.addEventListener('load', app);
 var Film = function(options){
   this.title = options.title;
   this.actors = options.actors;
-  this.genres = options.genres;
+  this.genres = options.genres || [];
   this.reviews = options.reviews || [];
 }
 
--- client/src/models/film.js
+++ client/src/models/film.js
@@ -1,7 +1,7 @@
 var Film = function(options){
   this.title = options.title;
   this.actors = options.actors;
-  this.genres = options.genres;
+  this.genres = options.genres || [];
   this.reviews = options.reviews || [];
 }
 
